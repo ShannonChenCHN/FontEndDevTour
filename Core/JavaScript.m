4885bfd4abfa428e20c34ddd7da2585f6e791319
Shannon Chen <273879688@qq.com>
2024-11-09 17:45:37 +0800

    Update JavaScript.m

--- Core/JavaScript.m
+++ Core/JavaScript.m
@@ -1,1 +1,129 @@
+# JavaScript
 
+## 学习大纲
+### 一、[JavaScript 简介](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/51) 
+1. JavaScript 简介
+- JavaScript 是什么
+- JavaScript 的历史
+- [ECMAScript 2015（ES6）](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/34)
+- JavaScript 的超集：[TypeScript](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/69)
+
+2. JavaScript 的应用
+
+- 前端开发
+  - web 前端/h5
+  - 原生 App/React Native
+- 后端服务器开发（Node.js）
+
+### 二、开发环境、工具以及调试
+- [Chrome DevTools](https://developers.google.com/web/tools/)
+- VSCode
+- [npm](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/46)
+- Node.js
+- 在线工具
+    - [https://jsfiddle.net](https://jsfiddle.net)
+    - [CodePen](https://codepen.io/)
+
+### 三、[JavaScript 基础](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/53)
+1. Hello World
+  - JS 输出
+  - JS 语句
+  - 注释
+2. 变量和数据类型
+  - 变量
+    - 变量声明
+    - 变量命名
+    - 关键字和保留字 
+  - 数据类型
+    - 原始类型（Primitive Type）
+    - 引用类型
+    - 类型转换
+3. 运算符
+4. 条件和循环
+5. 函数和闭包
+- 函数
+- 变量作用域与解构赋值
+- 方法
+- arguments 对象
+- Function 对象
+- 高阶函数
+- [闭包（closure）](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/38)
+- 箭头函数
+6. generator
+
+### 四、标准库
+
+1. Object 对象
+2. 属性描述对象
+3. Array 对象
+4. 包装对象
+5. Boolean 对象
+6. Number 对象
+7. String 对象
+8. Math 对象
+9. Date 对象
+10. RegExp 对象
+11. JSON 对象
+
+### 五、面向对象
+1. 面向对象
+  - 对象类型
+  - 对象作用域
+  - [定义类或对象](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/39)
+  - [修改对象](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/40)
+  - [this 关键字以及 call, apply 和 bind 函数](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/12)
+2. 继承
+- [原型继承](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/41)
+- class 继承
+- super 关键字
+
+
+### 六、异步操作
+1. 概述
+2. 定时器
+3. [Promise](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/65)
+
+### 七、浏览器
+
+1. DOM（文档对象模型）
+2. 事件
+3. BOM（浏览器对象模型）
+4. [AJAX](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/60)
+5. Canvas
+
+### 八、错误处理
+
+### 九、underscore
+
+### 十、Node.js
+
+### 十一、库和框架
+1. JQuery
+2. React.js
+3. React Native
+
+### 十二、实战
+1. web 前端开发实战
+2. React Native 实战
+3. Node.js 实战
+3. 构建基于 Node.js 的命令行工具
+
+
+## 相关资源
+- [A re-introduction to JavaScript (JS tutorial)](https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript)（复习/回顾用）
+- 前端 JavaScript 教程
+  - [JavaScript 基础教程 - W3School](http://www.w3school.com.cn/js/index.asp)
+  - [ECMAScript 教程 - W3School](http://www.w3school.com.cn/js/index_pro.asp)
+  - [JavaScript 对象参考手册 - W3School](http://www.w3school.com.cn/jsref/index.asp)
+  - [Browser 对象参考手册 - W3School](http://www.w3school.com.cn/jsref/index.asp)
+  - [HTML DOM 对象参考手册 - W3School](http://www.w3school.com.cn/jsref/index.asp)
+- [JavaScript 教程 - 阮一峰](https://wangdoc.com/javascript/index.html)
+- [JavaScript教程- 廖雪峰的官方网站](https://www.liaoxuefeng.com/wiki/001434446689867b27157e896e74d51a89c25cc8b43bdb3000)
+- [Node.js](https://github.com/ShannonChenCHN/AFrontEndWebDevTour/issues/47)
+- [JS 函数式编程指南](https://llh911001.gitbooks.io/mostly-adequate-guide-chinese/content/)（[英文原版](https://github.com/MostlyAdequate/mostly-adequate-guide)）
+
+## 问题汇总
+- [什么是脚本语言？](https://zh.wikipedia.org/wiki/脚本语言)
+- [网站为什么 JS 调用尽量放到网页底部？](https://www.zhihu.com/question/34147508)
+- [JavaScript 中字符串变量使用单引号和双引号的利弊？](https://www.zhihu.com/question/21168673)
+- [在JS\JQ中，究竟何时用单引号''、双引号“”、和不用引号呢？](https://segmentfault.com/q/1010000004519527)
